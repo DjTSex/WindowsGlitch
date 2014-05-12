54ccfd93da6f66a78b088f90ff2658eed2762f94
Timmy <Timmy@Tim-PC>
2014-05-11 20:26:45 -0400

    new commit

---
+++ src/windowsglitch/WindowsGlitch.java
@@ -0,0 +1,20 @@
+/*
+ * Tim Sexton
+ * Windows Glitch Application
+ * Purpose: Create an application that can recreate an old windows visual glitch
+ * for artistic purposes.
+ */
+package windowsglitch;
+
+//Import Statements
+
+
+public class WindowsGlitch {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
