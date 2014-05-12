530f801fd3d9b1f5eeb08d1e502ab23c2332ec7c
Timmy <Timmy@Tim-PC>
2014-05-11 20:54:09 -0400

    Makes an empty java application with a title

--- src/windowsglitch/WindowsGlitch.java
+++ src/windowsglitch/WindowsGlitch.java
@@ -8,13 +8,38 @@ package windowsglitch;
 
 //Import Statements
 
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.JFrame;
+import java.util.List;
+import java.util.ArrayList; 
+import javax.swing.JLabel;
+import javax.swing.JButton;
 
 public class WindowsGlitch {
-
-    /**
-     * @param args the command line arguments
-     */
+    //Variables
+    private static final int APPLICATION_WIDTH  = 800;      // Width, in pixels
+    private static final int APPLICATION_HEIGHT = 600;      // Height, in pixels
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        // Create a new application
+        JFrame application = new JFrame();
+        
+        // Set default close operation for the application
+        application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        // Set application size
+        application.setSize(APPLICATION_WIDTH, APPLICATION_HEIGHT);
+        
+        // Prevent resizing
+        application.setResizable(false);
+        
+        // Set application title
+        application.setTitle("Trailing Windows Glitch Creator");
+        
+        // Make application visible
+        application.setVisible(true);
+        
     }
 }
